8ce1e3efb92a8f5c95e79b2865be373ef7ac048b
scottmotte <scott@scottmotte.com>
2013-11-18 17:22:00 -0800

    Shows example of how to use it

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ This is an example of using the [SendGrid Java library](https://github.com/sendg
 ```bash
 git clone http://github.com/scottmotte/sendgrid-java-example.git
 cd sendgrid-java-example
+gradle build # this might be optional
 export TO=your@email.com SENDGRID_USERNAME=your_username SENDGRID_PASSWORD=your_password
-gradle build
+java -cp repo/com/github/scottmotte/0.0.1/scottmotte-0.0.1-jar.jar com.github.scottmotte.SendGridJavaExample
 ```
+
+You should get a `{"message":"success"}` response back if your credentials were correct.
--- src/main/java/com/github/scottmotte/SendGridJavaExample.java
+++ src/main/java/com/github/scottmotte/SendGridJavaExample.java
@@ -4,7 +4,7 @@ import java.lang.*;
 import com.github.sendgrid.SendGrid;
 
 public class SendGridJavaExample {
-  public static void main() {
+  public static void main(String[] args) {
     String sendgrid_username  = "motte777"; //System.getenv("SENDGRID_USERNAME");
     String sendgrid_password  = "motte777"; //System.getenv("SENDGRID_PASSWORD");
     String to                 = "motte@sendgrid.com"; //System.getenv("TO");
@@ -12,12 +12,15 @@ public class SendGridJavaExample {
     SendGrid sendgrid = new SendGrid(sendgrid_username, sendgrid_password);
 
     sendgrid.addTo(to);
+    sendgrid.addToName("Owl");
     sendgrid.setFrom(to);
+    sendgrid.setFromName("Owl");
+    sendgrid.setReplyTo("no-reply@owl.com");
     sendgrid.setSubject("[sendgrid-java-example] Owl");
     sendgrid.setHtml("<strong>Owl are you doing?</strong>");
     sendgrid.setText("Owl are you doing?");
 
-    sendgrid.send();
+    String response = sendgrid.send();
+    System.out.println(response);
   }
 }
-
